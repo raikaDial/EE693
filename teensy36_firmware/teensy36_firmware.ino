2a2424bd07d6c929b0698c7c99b3548835d9b932
Ryker Dial <ryker.dial@gmail.com>
2016-10-13 20:08:56 -0800

    Filtering is now windowed.

--- teensy36_firmware/teensy36_firmware.ino
+++ teensy36_firmware/teensy36_firmware.ino
@@ -19,8 +19,6 @@ FILINFO fno;      /* File information object */
 UINT bw, br;
 char buff[128];
 TCHAR wbuff[128];
-char *val2_ptr;
-int comma_idx = -1;
 
 /* Stop with dying message */
 void die(const char *text, FRESULT rc) {
@@ -32,9 +30,8 @@ TCHAR * char2tchar( char * charString, size_t nn, TCHAR * tcharString) {
   return tcharString;
 }
 
-char * tchar2char(  TCHAR * tcharString, size_t nn, char * charString)
-{ int ii;
-  for(ii = 0; ii<nn; ii++) charString[ii] = (char) tcharString[ii];
+char * tchar2char(  TCHAR * tcharString, size_t nn, char * charString){
+  for(size_t ii = 0; ii<nn; ii++) charString[ii] = (char) tcharString[ii];
   return charString;
 }
 // ********** //
@@ -46,57 +43,69 @@ ADXL345 adxl = ADXL345(10);
 // **********
 
 void setup() {
-  // Wait for serial to open. No need for Serial.begin() because Teensy has native USB
-  while (!Serial);
+    // Wait for serial to open. No need for Serial.begin() because Teensy has native USB
+    while (!Serial);
+  
+    f_mount(&fatfs, (TCHAR*)_T("/"), 0); /* Mount/Unmount a logical drive */
 
-  f_mount(&fatfs, (TCHAR*)_T("/"), 0); /* Mount/Unmount a logical drive */
+    // Overwrite existing output file if it exists
+    rc = f_open(&fil, (TCHAR*)_T("samples_filtered.csv"), FA_WRITE | FA_CREATE_ALWAYS);
+    if (rc) die("Open", rc); // Exit if file failed to open
+    rc = f_close(&fil);
+    if (rc) die("Close", rc);
+  
+    size_t numsamples_total = 21600;
+    size_t numsamples_window = 50;
+    float* ecg_data = new float[numsamples_window];
 
-  rc = f_open(&fil, (TCHAR*) _T("samples.txt"), FA_READ);
-  if (rc) die("Open",rc);
-
-  Serial.println("Get the file content.");
-  float ecg_data[2048];
-  uint16_t length = 2048;
-  for (int i=0; i<length; ++i) 
-  {
-      if(!f_gets(wbuff, sizeof(wbuff), &fil)) break; // Read a line from the csv file
-      tchar2char(wbuff, 128, buff); // Convert to char array
-      // Search line for comma. This method assumes only two values per line in csv.
-      for(int i=0; i<sizeof(buff); ++i) {
-        if(buff[i] == ',') { 
-          val2_ptr = buff + i + 1;
-          break;
+    // Read in all the ECG data and filter it
+    FSIZE_t file_idx_input = 0;
+    FSIZE_t file_idx_output = 0;
+    char *val2_ptr;
+    for(size_t i=0; i<numsamples_total; i+=numsamples_window) {
+        rc = f_open(&fil, (TCHAR*) _T("samples.csv"), FA_READ);
+        if (rc) die("Open",rc);
+        f_lseek(&fil,file_idx_input);
+        for (size_t j=0; j<numsamples_window; ++j) {
+            if(!f_gets(wbuff, sizeof(wbuff), &fil)) break; // Read a line from the csv file
+            tchar2char(wbuff, 128, buff); // Convert to char array
+            // Search line for comma. This method assumes only two values per line in csv.
+            for(size_t k=0; k<sizeof(buff); ++k) {
+              if(buff[k] == ',') { 
+                val2_ptr = buff + k + 1;
+                break;
+              }
+              else if(buff[k] == '\n')
+                 break;
+            }
+            float val2 = atof(val2_ptr);
+            ecg_data[j] = val2;
         }
-        else if(buff[i] == '\n')
-           break;
-      }
-      //float val1 = atof(buff);
-      float val2 = atof(val2_ptr);
-      ecg_data[i] = val2;
-//      Serial.printf("%f\n",val1);
-      Serial.printf("%f\n",val2);
-  }
-  rc = f_close(&fil);
-  if (rc) die("Close", rc);
-  ecg_filter.pan85_filter(ecg_data, length);
-  rc = f_open(&fil, (TCHAR*)_T("samples_filtered.csv"), FA_WRITE | FA_CREATE_ALWAYS);
-  if (rc) die("Open", rc); // Exit if file failed to open
-  for (int i = 0; i < length; ++i) {
-    sprintf(buff, "%f,\n", ecg_data[i]);
-    char2tchar(buff, 128, wbuff);
-    bw = f_puts(wbuff, &fil);
-  }
+        file_idx_input = f_tell(&fil);
+        rc = f_close(&fil);
+        if (rc) die("Close", rc);
 
-//  // How to close the file
-  rc = f_close(&fil);
-  if (rc) die("Close", rc);
-  Serial.println("All done!");
-//  Serial.println("All Done!");
-
-  // Setup ADXL345
-  adxl.setSpiBit(0); // Use 4-wire SPI
-  adxl.powerOn();
-  adxl.setRangeSetting(16);
+        ecg_filter.pan85_filter(ecg_data, numsamples_window);
+        rc = f_open(&fil, (TCHAR*)_T("samples_filtered.csv"), FA_WRITE | FA_OPEN_EXISTING);
+        if (rc) die("Open", rc); // Exit if file failed to open
+        f_lseek(&fil,file_idx_output);
+        
+        for (int i = 0; i < numsamples_window; ++i) {
+            sprintf(buff, "%f,\n", ecg_data[i]);
+            char2tchar(buff, 128, wbuff);
+            bw = f_puts(wbuff, &fil);
+        }
+        file_idx_output = f_tell(&fil);
+        rc = f_close(&fil);
+        if (rc) die("Close", rc);
+    }
+    delete [] ecg_data;
+    Serial.println("All done!");
+  
+    // Setup ADXL345
+    adxl.setSpiBit(0); // Use 4-wire SPI
+    adxl.powerOn();
+    adxl.setRangeSetting(16);
 }
 
 void loop() {
