3623443b15fe1960299eb7c76610e73e3143ca14
Ryker Dial <ryker.dial@gmail.com>
2016-11-02 23:22:29 -0800

    Logs data from ecg sensor to uSD.

--- teensy36_firmware/teensy36_firmware.ino
+++ teensy36_firmware/teensy36_firmware.ino
@@ -2,13 +2,22 @@
 // EE693 Fall 2016
 // Firmware for Teensy 3.6
 
-// ***** DSP Stuff ***** //
 #define ARM_MATH_CM4
 #define __FPU_PRESENT 1
 #include <arm_math.h>
 #include "ECG_Filter.h"
 #include "Filters.h"
+#include <SPI.h>
+#include <ADC.h>
+//#include <IntervalTimer.h>
+#include <TimerThree.h>
+#include <wchar.h>
+#include "ff.h"
+#include <SparkFun_ADXL345.h>
+#include "sleep.h"
+#include "clocks.h"
 
+// ***** DSP Stuff ***** //
 ECG_Filter ecg_filter;
 // Sign on 'a' coefficients is opposite of convention. Enter with caution.
 iir_filter notch60Hz( (const float32_t[]){0.9582, 0.9582, 0.9582, 0.9582, 0.9163}, 1 );
@@ -16,52 +25,36 @@ iir_filter emgLP( (const float32_t[]){0.1453, 0.2906, 0.1453, 0.6710, -0.2533}, 
 // ********** //
 
 // ***** Sensor Sampling Stuff ***** //
-#include <SPI.h>
-#include <ADC.h>
-#include <IntervalTimer.h>
 ADC *adc = new ADC(); // adc object
-
-IntervalTimer ecg_timer;
-const float scale_timer = 40.8; // Divide clock for lower frequency of sleep.
+const float scale_timer = 11.7;
 const uint32_t ECG_TS = (uint32_t)(1000000/360.0/scale_timer); // Sampling Period in Microseconds
 
 float32_t emg_fs = 250;
 const int ECG_PIN = A1;
-bool ecg_flag = 0;
+volatile bool ecg_flag = 0;
 // ********** //
 
-// ***** MicroSD Card Stuff ***** //
-#include <wchar.h>
-#include "ff.h"
 
+// ***** MicroSD Card Stuff ***** //
 FRESULT rc;        /* Result code */
 FATFS fatfs;      /* File system object */
 FIL fil;        /* File object */
 DIR dir;        /* Directory object */
 FILINFO fno;      /* File information object */
-UINT bw, br;
-char buff[128];
-TCHAR wbuff[128];
 
-TCHAR * char2tchar( char * charString, size_t nn, TCHAR * tcharString) {
-    for (size_t ii = 0; ii < nn; ii++) tcharString[ii] = (TCHAR) charString[ii];
-    return tcharString;
-}
-
-char * tchar2char(  TCHAR * tcharString, size_t nn, char * charString) {
-    for(size_t ii = 0; ii<nn; ii++) charString[ii] = (char) tcharString[ii];
-    return charString;
-}
+#define BUFFSIZE (2*1024)//(32*1024) // size of buffer to be written
+#define ECG_BUFFSIZE 1024
+uint8_t buffer[BUFFSIZE] __attribute__( ( aligned ( 16 ) ) );
+uint16_t ecg_buff[ECG_BUFFSIZE] __attribute__( ( aligned ( 16 ) ) );
+uint16_t ecg_buff_idx = 0;
 // ********** //
 
 // ***** ADXL345 Stuff ***** //
-#include <SparkFun_ADXL345.h>
 const uint16_t ADXL_CS_PIN = 10;
 ADXL345 adxl = ADXL345(10);
 // ********** //
 
 // ***** Low Power Mode Stuff ***** //
-#include <Snooze.h> // Using only for internal functions
 void sleep() {
     pee_blpi( );
     enter_vlpr( );
@@ -73,7 +66,7 @@ void sleep() {
 
 void setup() {
     // Wait for serial to open. No need for Serial.begin() because Teensy has native USB
-    while (!Serial);
+    //while (!Serial);
     pinMode(23, OUTPUT);
     f_mount(&fatfs, (TCHAR*)_T("/"), 0); /* Mount/Unmount a logical drive */
 
@@ -89,19 +82,38 @@ void setup() {
     adxl.powerOn();
     adxl.setRangeSetting(16);
     adxl.setImportantInterruptMapping(1, 1, 1, 1, 1); // Put all interrupts on pin 1
+
+    rc = f_open(&fil, (TCHAR*)_T("test.bin"), FA_WRITE | FA_CREATE_ALWAYS);
+    rc = f_close(&fil);
+
+    Timer3.initialize(ECG_TS);
+    Timer3.attachInterrupt(ECG_ISR);
 }
 
 void loop() {
     while(true) {
-        ecg_timer.begin(ECG_ISR, ECG_TS);
         sleep();
         noInterrupts();
-        digitalWriteFast(23, HIGH);
+        //digitalWriteFast(23, HIGH);
         if(ecg_flag) {
-          uint16_t val = adc->adc0->analogRead(ECG_PIN);
-          ecg_flag = false;
+            ecg_buff[ecg_buff_idx++%ECG_BUFFSIZE] = adc->adc0->analogRead(ECG_PIN);
+            ecg_flag = false;
         }
-        digitalWriteFast(23, LOW);
+        if (ecg_buff_idx%ECG_BUFFSIZE == 0) {
+          digitalWriteFast(23, HIGH);
+            rc = f_open(&fil, (TCHAR*)_T("test.bin"), FA_WRITE | FA_OPEN_EXISTING);
+            rc = f_lseek(&fil, f_size(&fil));
+            
+            for(int i=0; i<ECG_BUFFSIZE; ++i) {
+                buffer[2*i] = (uint8_t)(ecg_buff[i]);
+                buffer[2*i+1] = (uint8_t)(ecg_buff[i] >> 8);
+            }
+            rc = f_write(&fil, buffer, BUFFSIZE, &wr);
+            rc = f_close(&fil);
+            digitalWriteFast(23, LOW);  
+        }
+       
+        //digitalWriteFast(23, LOW);
         interrupts();
     }
 }
@@ -129,7 +141,6 @@ void ADXL_ISR() {
 }
 
 void ECG_ISR(void) {
-    ecg_timer.end();
     ecg_flag = true;
 }
 
